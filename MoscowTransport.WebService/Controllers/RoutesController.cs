cfd8b31e145321baa842d924ba734641b7f7acd1
Evgeniy-Kn <knyaz0520@mail.ru>
2020-05-15 22:49:19 +0300

    laba1

---
+++ MoscowTransport.WebService/Controllers/RoutesController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using SportsGround.DomainObjects;
+
+namespace SportsGround.WebService.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class RoutesController : ControllerBase
+    {
+        private readonly ILogger<RoutesController> _logger;
+
+        public RoutesController(ILogger<RoutesController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IEnumerable<NameFacility> GetRoute()
+        {
+            return new List<NameFacility>() 
+            { 
+                new NameFacility() 
+                { 
+                    Id = 1,
+                    TypeofSportsGround = "748",
+                    Name = "Ст. Ховрино - Ст. МЦД Бескудниково",
+                    Type = TransportType.Bus,
+                    Organization = new TransportOrganization()
+                    {
+                        Id = 1,
+                        Name = "ГУП \"Мосгортранс\"",
+                        TimeZone = "Europe/Moscow",
+                        WebSite = "http://mosgortrans.ru"
+                    }
+                } 
+            };
+        }
+    }
+}
---
+++ MoscowTransport.WebService/DomainObjects/SportsGround.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SportsGround.DomainObjects
+{
+    public class NameFacility : DomainObject
+    {
+        public string TypeofSportsGround { get; set; }
+
+        public string AdministrativeDistrict { get; set; }
+
+        public string Area { get; set; }
+
+        public string Address { get; set; }
+
+        public string WebSite { get; set; }
+
+        public string Email { get; set; }
+
+        public string NumberPhone { get; set; }
+
+        public string Lighting { get; set; }
+
+        public string Name { get; set; }
+
+
+    }
+}
